ea45cedf810b91268b44924833e00de0ab032379
qwanysh <olimpsdu@gmail.com>
2021-11-04 11:33:41 +0600

    Add config

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AWS_BUCKET = os.getenv('AWS_BUCKET')
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
--- main.py
+++ main.py
@@ -3,25 +3,31 @@ import os
 
 import boto3
 from botocore.exceptions import ClientError
-from dotenv import load_dotenv
 
-load_dotenv()
+import config
 
 s3_client = boto3.client(
-    service_name='s3', aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    service_name='s3',
+    aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY,
 )
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'action', type=action,
+        'action',
+        type=action,
         help=f"action({', '.join(actions)}) to perform",
     )
-    parser.add_argument('file_name', type=file_name, help='file to upload')
     parser.add_argument(
-        '-k', '--key',
+        'file_name',
+        type=file_name,
+        help='file to upload',
+    )
+    parser.add_argument(
+        '-k',
+        '--key',
         help='S3 key to upload. If not specified then file_name is used',
     )
     return parser.parse_args()
@@ -30,8 +36,10 @@ def get_args():
 def upload(args):
     try:
         s3_client.upload_file(
-            args.file_name, os.getenv('AWS_BUCKET'),
+            args.file_name,
+            config.AWS_BUCKET,
             args.key or args.file_name,
+            # ExtraArgs={'ContentType': 'image/svg+xml'},
         )
     except ClientError as e:
         print(e)
@@ -43,6 +51,7 @@ actions = {'upload': upload}
 def action(value):
     if value in actions:
         return value
+
     raise argparse.ArgumentTypeError(
         f"use one of ({', '.join(actions)})",
     )
@@ -51,12 +60,20 @@ def action(value):
 def file_name(value):
     if os.path.isfile(value):
         return value
+
     raise argparse.ArgumentTypeError(f'"{value}" file does not exist')
+
+
+def confirm_action():
+    confirmation = input(f'Using {config.AWS_BUCKET} bucket. Y/N? ')
+    return confirmation.lower() == 'y'
 
 
 def main():
     args = get_args()
-    actions[args.action](args)
+
+    if confirm_action():
+        actions[args.action](args)
 
 
 if __name__ == '__main__':
